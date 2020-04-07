0c3bac716d37d24e871a7ee23818b4b85b2e3763
Tomasz Kawczynski <tomasz.kawczynski@gmail.com>
2020-04-07 12:42:06 +0200

    Added remote login to database

--- provision/install_mysql.sh
+++ provision/install_mysql.sh
@@ -5,7 +5,11 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password password pas
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password pass'
 apt-get update
 apt-get install -y mysql-server
-	
+
+sed -i "s/bind-address.*/bind-address = 0.0.0.0/g" /etc/mysql/mysql.conf.d/mysqld.cnf
+service mysql restart
+
+mysql -uroot -ppass -e"GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'pass';"
 mysql -uroot -ppass -e"CREATE DATABASE wp_db;"
 mysql -uroot -ppass -e"GRANT ALL ON wp_db.* to 'wp_user'@'%' identified by 'wp_pass';"
 
